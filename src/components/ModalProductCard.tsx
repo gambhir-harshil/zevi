8de77d8d445e0ef20d89674704e5860aeeb052dd
gambhir-harshil <gambhirsahab3@gmail.com>
2023-12-27 23:21:18 +0530

    add hover effects

--- src/components/ModalProductCard.tsx
+++ src/components/ModalProductCard.tsx
@@ -7,14 +7,14 @@ const ModalProductCard: React.FC<ModalProductCardProps> = ({ product }) => {
   console.log(product);
   return (
     <div className="flex flex-col gap-2">
-      <div className="bg-cover w-[160px] h-[180px]">
+      <div className="bg-cover w-[160px] h-[180px] hover:translate-x-1 hover:translate-y-1 transition ease-linear">
         <img
+          loading="lazy"
           src={product.imageUrl}
           className="h-full w-full"
           alt={product.name}
         />
       </div>
-
       <span className="text-sm font-medium">{product.name}</span>
     </div>
   );
