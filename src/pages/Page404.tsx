fbd408f1b4d20b94ea5045a4f34e440b0c7551d0
gambhir-harshil <gambhirsahab3@gmail.com>
2023-12-29 02:36:21 +0530

    readme

--- src/pages/Page404.tsx
+++ src/pages/Page404.tsx
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+
 const Page404 = () => {
-  return <>OOPS! Seems like you're lost</>;
+  return (
+    <div className="flex items-center justify-center h-screen text-white bg-black">
+      <h1 className="text-4xl">OOPS! Seems like you're lost</h1>
+      <Link to="/" className="text-xl underline">
+        Home
+      </Link>
+    </div>
+  );
 };
 
 export default Page404;
